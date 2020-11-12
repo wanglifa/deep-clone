984f6036886c8dd951646ad2fe3cf2d4b7ef8fd3
wanglifa <wanglifa1995@gmail.com>
2020-11-12 16:48:13 +0800

    简单实现深拷贝

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-function deepCLone() {
-
+function deepClone(source) {
+    if (source instanceof Object) {
+        let deepObj
+        if (source instanceof Array) {
+            deepObj = new Array()
+        } else if (source instanceof Function) {
+            deepObj = function() {
+                return source.apply(this, arguments)
+            }
+        } else {
+            deepObj = new Object()
+        }
+    
+        for (let key in source) {
+            // 对对象里的每一项进行深拷贝并把这一项赋值给新的对象
+            deepObj[key] = deepClone(source[key])
+        }
+        return deepObj
+    }
+    return source
 }
-module.exports =  deepCLone
+module.exports =  deepClone
--- test/index.js
+++ test/index.js
@@ -1,12 +1,60 @@
 const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
-const deepCLone = require('../src/index')
 chai.use(sinonChai)
 const assert = chai.assert
 const deepClone = require('../src/index')
 describe('deepClone', () => {
     it('是一个函数', () => {
-        assert.isFunction(deepCLone)
+        assert.isFunction(deepClone)
+    })
+    it('可以复制基本类型', () => {
+        const n = 123
+        const n2 = deepClone(n)
+        assert(n ===n2)
+        const b = '123456'
+        const b2 = deepClone(b)
+        assert(b === b2)
+        const c = undefined
+        const c2 = deepClone(c)
+        assert(c === c2)
+        const d = null
+        const d2 = deepClone(d)
+        assert(d === d2)
+        const e = true
+        const e2 = deepClone(e)
+        assert(e === e2)
+        const f = Symbol()
+        const f2 = deepClone(f)
+        assert(f === f2)
+    })
+    it('可以复制普通对象', () => {
+        const a = { name: '立发', child: { name: '小立发'}}
+        const a2 = deepClone(a)
+        assert(a !== a2)
+        assert(a.name === a2.name)
+        assert(a.child !== a2.child)
+        assert(a.child.name === a2.child.name)
+    })
+    it('可以复制数组对象', () => {
+        const a = [[1, 2], [4, 5], [6, 7]]
+        const a2 = deepClone(a)
+        assert(a !== a2)
+        assert(a[0] !== a2[0])
+        assert(a[1] !== a2[1])
+        assert(a[2] !== a2[2])
+        assert.deepEqual(a, a2)
+    })
+    it('可以复制函数', () => {
+        const a = function(x, y) {
+            return x + y
+        }
+        a.xxx = { yyy: { zzz: 1 } }
+        const a2 = deepClone(a)
+        assert(a !== a2)
+        assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz)
+        assert(a.xxx.yyy !== a2.xxx.yyy)
+        assert(a.xxx !== a2.xxx)
+        assert(a(1,2) === a2(1,2))
     })
 })
